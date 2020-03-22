fe1bfcf5f08c742bc95ccca75f3a5beb64b0c7c0
Nicky Bangs <nickybangs@Nickys-Air.home>
2020-03-22 00:09:28 -0400

    add normalization

--- app/src/main.py
+++ app/src/main.py
@@ -2,16 +2,33 @@
 #                              APP V1                                #
 ######################################################################
 
-import fire
 from pathlib import Path
+
+import fire
 from PIL import Image
+from torch import Tensor
 
-ROOT_PATH = Path('/Users/nickybangs/gh/image_to_text')
+ROOT_PATH = Path("/Users/nickybangs/gh/image_to_text")
+DEFAULT_IM_PATH = ROOT_PATH / "app" / "src" / "lib" / "input"
 
-def main(image):
-    im = Image.open(ROOT_PATH/'app'/'src'/'lib'/'input'/image)
-    im.convert('L')
-    im.show()
 
-if __name__ == '__main__':
+# double check what preprocessing they do in fastai..
+# this would be the entry point for making sure image looks correct
+def preprocess_image(im: Image) -> Tensor:
+    """
+    Process the given Image to match expected characteristics of input.
+    Return a Tensor of the image data
+    """
+    im.convert("L")
+    image_tensor = Tensor(im.getdata())
+    image_tensor.div_(255)
+    return image_tensor
+
+
+def main(image: str, image_path=Path(DEFAULT_IM_PATH)):
+    im = Image.open(image_path / image)
+    return preprocess_image(im)
+
+
+if __name__ == "__main__":
     fire.Fire(main)
